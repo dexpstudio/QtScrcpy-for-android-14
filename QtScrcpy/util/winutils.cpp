f094cdb356477d25aa71f418b9ad234c2b3934c4
xfg <1432837709@qq.com>
2024-04-25 16:27:59 +0800

    *chore:保证mingw能够编译

--- QtScrcpy/util/winutils.cpp
+++ QtScrcpy/util/winutils.cpp
@@ -19,10 +19,11 @@ WinUtils::~WinUtils(){};
 // Reference: qt/qtbase.git/tree/src/plugins/platforms/windows/qwindowswindow.cpp
 bool WinUtils::setDarkBorderToWindow(const HWND &hwnd, const bool &d)
 {
-    const BOOL darkBorder = d ? TRUE : FALSE;
-    const bool ok = SUCCEEDED(DwmSetWindowAttribute(hwnd, DwmwaUseImmersiveDarkMode, &darkBorder, sizeof(darkBorder)))
-                    || SUCCEEDED(DwmSetWindowAttribute(hwnd, DwmwaUseImmersiveDarkModeBefore20h1, &darkBorder, sizeof(darkBorder)));
-    if (!ok)
-        qWarning("%s: Unable to set dark window border.", __FUNCTION__);
-    return ok;
+//    const BOOL darkBorder = d ? TRUE : FALSE;
+//    const bool ok = SUCCEEDED(DwmSetWindowAttribute(hwnd, DwmwaUseImmersiveDarkMode, &darkBorder, sizeof(darkBorder)))
+//                    || SUCCEEDED(DwmSetWindowAttribute(hwnd, DwmwaUseImmersiveDarkModeBefore20h1, &darkBorder, sizeof(darkBorder)));
+//    if (!ok)
+//        qWarning("%s: Unable to set dark window border.", __FUNCTION__);
+//    return ok;
+    return false;
 }
